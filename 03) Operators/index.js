01c596f46bf7a87f7b9922df65cfed245519184e
Sahil Patil <71595574+sahilpatil9763@users.noreply.github.com>
2021-07-31 16:27:11 +0530

    Operators

---
+++ 03) Operators/index.js
@@ -0,0 +1,39 @@
+var x=52, y=10;
+x = x%y;
+
+
+var a=10, b=2;
+a = a**b;
+
+
+var x=3 == 3;
+
+// Not Equal Sign
+
+var x=3 != '3';
+
+// Greater than & Greater than equal to
+
+var x=3 <= '3';
+
+// Increament
+
+var x=7;
+x++;
+
+// Arithmatic Operator In Javascript
+
+var num1 = 2;
+var num2 = 3;
+
+console.log("The Value of num1 + num2 is "+ (num1 + num2));
+console.log("The Value of num1 - num2 is "+ (num1 - num2));
+console.log("The Value of num1 * num2 is "+ (num1 * num2));
+console.log("The Value of num1 / num2 is "+ (num1 / num2));
+
+// Exponentiation Operator
+
+console.log("The Value of num1++ " + (num1++));
+console.log("The Value of ++num1 " + (++num1));
+console.log("The Value of num1-- " + (num1--));
+console.log("The Value of --num1 " + (--num1));
