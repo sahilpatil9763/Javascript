eb53ad0e2b31c899901b3cf2684eda7147afbba6
Sahil Patil <71595574+sahilpatil9763@users.noreply.github.com>
2021-08-01 11:11:18 +0530

    Strings

---
+++ 04) Strings/demo.js
@@ -0,0 +1,15 @@
+var string = 'this';
+var name = ' Sahil ';
+var fullname = 'Sahil Mahadev Patil';
+var message = ' My Name is Sahil Patil '
+var temp = `${name} is a nice Person and his full name is ${fullname}`;
+
+console.log(string + name + message);
+console.log(temp);
+
+var len = name.length;
+console.log(`Length of name is ${len}`);
+
+console.log("Hello World \n Sahil");
+
+document.getElementById('content').innerHTML = '<h3> This is An Heading</h3>'
