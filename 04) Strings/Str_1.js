c61f9a292d5054e41e6de69e7ae0bdc5e94ee5bd
Sahil Patil <71595574+sahilpatil9763@users.noreply.github.com>
2024-02-14 22:46:54 +0530

    Create Str_1.js

---
+++ 04) Strings/Str_1.js
@@ -0,0 +1,13 @@
+let name = "Sahil"
+let friend = "Uday"
+console.log("His name is " +name+ " and his friends name is "+friend);
+
+console.log(`His name is ${name} and his friends name is ${friend}`);
+
+// Two ways of printing the string
+
+
+let b = "Shivam"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
