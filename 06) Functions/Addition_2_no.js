e9cc403ce5dca6d28162194f98f7d0cf4deecefc
Sahil Patil <71595574+sahilpatil9763@users.noreply.github.com>
2024-02-14 22:22:29 +0530

    Create Addition_2_no.js

---
+++ 06) Functions/Addition_2_no.js
@@ -0,0 +1,16 @@
+function sum(a,b)
+{
+    //console.log(a + b);
+    return a + b;
+}
+
+result = sum(3,5);
+result1 = sum(4,5);
+result2 = sum(7,5);
+
+
+console.log("The sum of these numbers is:"+result);
+
+console.log("The sum of these numbers is:"+result1);
+
+console.log("The sum of these numbers is:"+result2);
