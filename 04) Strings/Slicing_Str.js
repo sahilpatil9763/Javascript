d0120c3eb081e4de697f37978930255fa05cc2ab
Sahil Patil <71595574+sahilpatil9763@users.noreply.github.com>
2024-02-14 22:54:03 +0530

    Create Slicing_Str.js
    Strings

---
+++ 04) Strings/Slicing_Str.js
@@ -0,0 +1,12 @@
+let a = "Sahil"
+let b = "Shivam"
+console.log(b.slice(1,4))
+console.log(b.slice(1))
+
+//Replacing str
+
+console.log(b.replace("Sh","77"))
+
+// Concatination
+
+console.log(b.concat(a, "Seema", "Rahul", "Priya"))
