23825e693ddc70350c30bb9fe693f01e5b2daa6e
Sahil Patil <71595574+sahilpatil9763@users.noreply.github.com>
2025-05-08 13:08:28 +0530

    Create 03_dataTypes.js
    We will learn about data types in Javascript

---
+++ Javascript Practice/01_Basics/03_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello")
+
+
+let name = "Sahil";
+let age = 24;
+let isLoggedIn = false;
+
+// number => 2 to the power of 53
+// bigint
+// string => ""
+// boolean => true or false
+// null => standalone value. It is a representation of empty value. But its not like undefined.
+// undefined => It means value is not assigned
+// symbol => it is to define uniqueness
+
+
+// object
+
+console.log(typeof null);  // When we run null it defines as Object  *IMP*
+console.log(typeof undefined);
+
