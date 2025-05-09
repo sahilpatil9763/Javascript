0d7093d52e87bd14dcbbcb6ab731946e101cd08f
Sahil Patil <71595574+sahilpatil9763@users.noreply.github.com>
2025-05-09 16:34:06 +0530

    Create 04_conversionOperation.js
    Conversion of Data types

---
+++ Javascript Practice/01_Basics/04_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = "Sahil";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(33);
+let valueInNumber2 = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber2);
+
+//console.log(valueInNumber2);
+
+//  "33" => 33
+//  "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+//  1 => true; 0 => false
+//  "" => false
+//  "Sahil" => true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
